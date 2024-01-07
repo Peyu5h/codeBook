3c6750bb7df361bc35289425dc1d12b5a3b7eeec
Piyush Bagul <piyushvbagul916@gmail.com>
2024-01-07 23:29:55 +0530

    global context

--- src/context/FilterContext.jsx
+++ src/context/FilterContext.jsx
@@ -35,8 +35,5 @@ export const FilterProvider = ({ children }) => {
 
 export const useFilter = () => {
   const context = useContext(FilterContext);
-  if (!context) {
-    throw new Error("useFilter must be used within a FilterProvider");
-  }
   return context;
 };
