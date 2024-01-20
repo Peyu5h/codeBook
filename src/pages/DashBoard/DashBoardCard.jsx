bf0d80b0150a58d5a4b02de9c728d27b5bb02db6
Piyush Bagul <piyushvbagul916@gmail.com>
2024-01-20 22:10:24 +0530

    dashboard done

--- src/pages/DashBoard/DashBoardCard.jsx
+++ src/pages/DashBoard/DashBoardCard.jsx
@@ -1,11 +1,49 @@
-import Random from "../../assets/images/10002.avif";
+import { useEffect, useState } from "react";
 
-const DashBoardCard = () => {
+const DashBoardCard = ({ itemId }) => {
+  const [item, setItem] = useState({});
+
+  useEffect(() => {
+    const fetchItem = async () => {
+      const response = await fetch(`http://localhost:3001/products/${itemId}`);
+      const data = await response.json();
+      setItem(data);
+    };
+    fetchItem();
+  }, []);
+
+  const one =
+    "https://assets-global.website-files.com/64c4b66a44c38c5fa4309e5a/6536851236dce583259b23be_1-STAR.png";
+  const two =
+    "https://assets-global.website-files.com/64c4b66a44c38c5fa4309e5a/653685122b1f6a53600ba761_2-STAR.png";
+  const three =
+    "https://assets-global.website-files.com/64c4b66a44c38c5fa4309e5a/653685128e91f8427cb55311_3-STAR.png";
+  const four =
+    "https://assets-global.website-files.com/64c4b66a44c38c5fa4309e5a/64c4d9e4a124713eeaa01f27_4-STAR.png";
+  const five =
+    "https://assets-global.website-files.com/64c4b66a44c38c5fa4309e5a/653685129ff29a55d5b1abdd_5-STAR.png";
+
+  const [rating, setRating] = useState();
+
+  useEffect(() => {
+    if (item.rating === 1) {
+      setRating(one);
+    } else if (item.rating === 2) {
+      setRating(two);
+    } else if (item.rating === 3) {
+      setRating(three);
+    } else if (item.rating === 4) {
+      setRating(four);
+    } else if (item.rating === 5) {
+      setRating(five);
+    }
+  }, [item.rating]);
+
   return (
     <main>
       <div className="flex gap-4 mx-auto bg-slate-700 my-4 rounded-md">
         <div className="img">
-          <img className="w-48 h-full rounded-l-md" src={Random} alt="" />
+          <img className="w-48 h-full rounded-l-md" src={item.poster} alt="" />
         </div>
 
         {/* Parent container with 'flex' class */}
@@ -14,19 +52,15 @@ const DashBoardCard = () => {
           <div className="left flex flex-col font-semibold md:gap-4 gap-1">
             <div className="flex flex-col gap-2">
               <div className="title md:text-xl text-xs font-semibold ">
-                The Complete Guide To Backend Development
+                {item.name}
               </div>
-              <img
-                className="w-24"
-                src="https://assets-global.website-files.com/64c4b66a44c38c5fa4309e5a/64c4d9e4a124713eeaa01f27_4-STAR.png"
-                alt="star"
-              />
+              <img className="w-24" src={rating} alt="star" />
             </div>
-            <div className="sm-price sm:hidden text-sm">$99</div>
+            <div className="sm-price sm:hidden text-sm">${item.price}</div>
           </div>
 
           {/* Right side content */}
-          <div className="right text-xl hidden sm:block">$99</div>
+          <div className="right text-xl hidden sm:block">${item.price}</div>
         </div>
       </div>
     </main>
--- src/pages/DashBoard/DashBoardPage.jsx
+++ src/pages/DashBoard/DashBoardPage.jsx
@@ -1,6 +1,21 @@
 import DashBoardCard from "./DashBoardCard";
 import Header from "../../components/Header";
+import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 const DashBoardPage = () => {
+  const [userData, setUserData] = useState({});
+  const user = useSelector((state) => state.user);
+  useEffect(() => {
+    const fetchUserDetails = async () => {
+      const response = await fetch(
+        `http://localhost:3001/userDetails/${user.id}`
+      );
+      const data = await response.json();
+      setUserData(data);
+    };
+    fetchUserDetails();
+  }, []);
+
   return (
     <div>
       <Header />
@@ -9,22 +24,42 @@ const DashBoardPage = () => {
           <div className="title mt-8 mb-2 text-2xl"> Dashboard</div>
           <div className="line h-[2px] w-[20%] bg-blue-500 mb-12"></div>
         </div>
+        {userData.order !== "" &&
+          userData.order !== null &&
+          userData.order !== undefined && (
+            <div className="orderWrapper p-4 border-2 border-slate-700 rounded-lg w-[100%] lg:w-[70%] mx-auto my-4">
+              <div className="orderIdTotal flex justify-between mb-6">
+                <h1 className="text-lg">Date: {userData.orderDate} </h1>
+                <h1 className="text-xl">Total: $128 </h1>
+              </div>
+              {userData.order &&
+                userData.order.map((item) => (
+                  <DashBoardCard key={item} itemId={item} />
+                ))}
+            </div>
+          )}
 
-        <div className="orderWrapper p-4 border-2 border-slate-700 rounded-lg w-[100%] lg:w-[70%] mx-auto my-4">
-          <div className="orderIdTotal flex justify-between mb-6">
-            <h1 className="text-lg">order id: 123456789</h1>
-            <h1 className="text-xl">Total: $123</h1>
-          </div>
-          <DashBoardCard />
-        </div>
-        <div className="orderWrapper p-4 border-2 border-slate-700 rounded-lg w-[100%] lg:w-[70%] mx-auto my-4">
-          <div className="orderIdTotal flex justify-between mb-6">
-            <h1 className="text-lg">order id: 123456789</h1>
-            <h1 className="text-xl">Total: $123</h1>
-          </div>
-          <DashBoardCard />
-          <DashBoardCard />
-        </div>
+        {userData &&
+          userData.oldOrders &&
+          userData.oldOrders.map(
+            (order) =>
+              order.items &&
+              order.items.length > 0 && (
+                <div
+                  key={order._id}
+                  className="orderWrapper p-4 border-2 border-slate-700 rounded-lg w-[100%] lg:w-[70%] mx-auto my-4"
+                >
+                  <div className="orderIdTotal flex justify-between mb-6">
+                    <h1 className="text-lg">Order ID: {order.date}</h1>
+                    <h1 className="text-xl">Total: ${order.total}</h1>
+                  </div>
+                  {order.items &&
+                    order.items.map((item) => (
+                      <DashBoardCard key={item} itemId={item} />
+                    ))}
+                </div>
+              )
+          )}
       </main>
     </div>
   );
