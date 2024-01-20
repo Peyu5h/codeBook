e5f2c002ecaa404b77942cfeee6f1289b9b729a6
Piyush Bagul <piyushvbagul916@gmail.com>
2024-01-20 17:06:25 +0530

    cart page

--- src/pages/Cart/CartList.jsx
+++ src/pages/Cart/CartList.jsx
@@ -1,29 +1,38 @@
-import Random from "../../assets/images/10002.avif";
-const CartList = () => {
+import { useAtom } from "jotai";
+import { carItemsAtom } from "../../reducer/atom";
+
+const CartList = ({ name, price, img }) => {
+  const [cartItems, setCartItems] = useAtom(carItemsAtom);
+
+  const handleRemove = () => {
+    setCartItems(cartItems.filter((item) => item.name !== name));
+  };
+
   return (
     <main>
       <div className="flex gap-4 mx-auto w-[100%] lg:w-[70%] bg-slate-700 my-4 rounded-md">
         <div className="img">
-          <img className="w-48 h-full rounded-l-md" src={Random} alt="" />
+          <img className="w-48 h-full rounded-l-md" src={img} alt="" />
         </div>
 
-        {/* Parent container with 'flex' class */}
+        {/* Parent */}
         <div className="content flex justify-between items-center p-2 w-full md:mr-4 mr-2">
           {/* Left side content */}
           <div className="left flex flex-col font-semibold md:gap-4 gap-1">
-            <div className="title md:text-xl text-xs font-semibold">
-              The Complete Guide To Backend Development
-            </div>
-            <div className="sm-price sm:hidden text-sm">$99</div>
+            <div className="title md:text-xl text-xs font-semibold">{name}</div>
+            <div className="sm-price sm:hidden text-sm">${price}</div>
             <div className="remove cursor-pointer ">
-              <span className=" text-red-600 rounded-full md:text-md text-xs">
+              <span
+                onClick={handleRemove}
+                className=" text-red-600 rounded-full md:text-md text-xs"
+              >
                 Remove
               </span>
             </div>
           </div>
 
           {/* Right side content */}
-          <div className="right text-xl hidden sm:block">$99</div>
+          <div className="right text-xl hidden sm:block">${price}</div>
         </div>
       </div>
     </main>
--- src/pages/Cart/CartPage.jsx
+++ src/pages/Cart/CartPage.jsx
@@ -1,25 +1,40 @@
 import CartList from "./CartList";
 import Header from "../../components/Header";
 import { FaArrowRightLong } from "react-icons/fa6";
+import { useAtom } from "jotai";
+import { carItemsAtom } from "../../reducer/atom";
+import { useState } from "react";
 
 const CartPage = () => {
+  const [cartItems, setCartItems] = useAtom(carItemsAtom);
+
+  const totalPrice = cartItems.reduce((acc, item) => acc + item.price, 0);
+
   return (
     <div>
       <Header />
       <main className=" lg:mt-4 mt-2 md:mx-24 mx-2">
         <div className=" flex flex-col items-center justify-center">
-          <div className="title mt-8 mb-2 text-2xl"> My Cart (12)</div>
+          <div className="title mt-8 mb-2 text-2xl">
+            {" "}
+            My Cart ({cartItems?.length})
+          </div>
           <div className="line h-[2px] w-[20%] bg-blue-500 mb-12"></div>
         </div>
-        <CartList />
-        <CartList />
-        <CartList />
-        <CartList />
+        {cartItems?.map((item) => (
+          <CartList
+            key={item._id}
+            name={item.name}
+            price={item.price}
+            img={item.poster}
+          />
+        ))}
+
         <div className="bottom mx-auto  w-full md:w-[70%]">
           <div className="divider h-[2px] w-full bg-slate-700 my-8 "></div>
           <div className="total flex justify-between">
             <div className="sm:text-xl text-lg">Total Amount:</div>
-            <div className="text-xl font-semibold mr-8">$128</div>
+            <div className="text-xl font-semibold mr-8">${totalPrice}</div>
           </div>
           <div className="divider h-[2px] w-full bg-slate-700 my-8 "></div>
 
